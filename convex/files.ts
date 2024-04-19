0290e424360a5868a9a84e1747c854255b955b5a
Ayush Jain <77970015+not4YU5H@users.noreply.github.com>
2024-04-19 13:07:23 +0530

    Added delete functionality

--- convex/files.ts
+++ convex/files.ts
@@ -36,7 +36,7 @@ export const createFile = mutation({
         );
 
         if(!hasAccess) {
-            throw new ConvexError("You do not have access to this org.")
+            throw new ConvexError("You do not have access to this org.");
         }
 
         await ctx.db.insert("files", {
@@ -70,3 +70,24 @@ export const getFiles = query({
         ).collect();
     },
 });
+
+export const deleteFile = mutation({
+    args : {fileId: v.id("files")},
+    async handler(ctx, args) {
+        const identity = await ctx.auth.getUserIdentity();
+        if(!identity) {
+            throw new ConvexError("You do not have access to this org.");
+        }
+        const file = await ctx.db.get(args.fileId);
+        if(!file) {
+            throw new ConvexError("This file does not exist");
+        }
+        const hasAccess = await hasAccessToOrg(
+            ctx, identity.tokenIdentifier, file.orgId
+        )
+        if(!hasAccess) {
+            throw new ConvexError("You do not have access to delete this file.");
+        }
+        await ctx.db.delete(args.fileId);
+    }
+})
